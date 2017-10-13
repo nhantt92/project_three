ddbc97ffa67a34e5f59dbe9078b241d2ebbbcbd1
victoriactemple <victoriactemple@gmail.com>
2017-10-13 15:24:46 -0400

    installed concurrently and set up in json package. changed express server to listen on port 3001

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.get('/', (req,res) => {
   res.send('Hello world!')
 })
 
-const PORT = process.env.PORT || 3000;
+// set app to listen on PORT 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
 })
